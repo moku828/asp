d560390f1b8c560f01a893806d7f1e1b9d8a083a
Masayuki Okumura <basashi@sannet.ne.jp>
2020-02-02 12:49:36 +0900

    Remove sample procedures

--- sample/sample1.c
+++ sample/sample1.c
@@ -282,18 +282,7 @@ void alarm_handler(intptr_t exinf)
  */
 void main_task(intptr_t exinf)
 {
-	char_t	c;
-	ID		tskid = TASK1;
-	int_t	tskno = 1;
 	ER_UINT	ercd;
-	PRI		tskpri;
-#ifndef TASK_LOOP
-	volatile ulong_t	i;
-	SYSTIM	stime1, stime2;
-#endif /* TASK_LOOP */
-#ifdef TOPPERS_SUPPORT_GET_UTM
-	SYSUTM	utime1, utime2;
-#endif /* TOPPERS_SUPPORT_GET_UTM */
 
 	SVC_PERROR(syslog_msk_log(LOG_UPTO(LOG_INFO), LOG_UPTO(LOG_EMERG)));
 	syslog(LOG_NOTICE, "Sample program starts (exinf = %d).", (int_t) exinf);
@@ -313,197 +302,14 @@ void main_task(intptr_t exinf)
 	SVC_PERROR(serial_ctl_por(TASK_PORTID,
 							(IOCTL_CRLF | IOCTL_FCSND | IOCTL_FCRCV)));
 
-	/*
- 	 *  ループ回数の設定
-	 *
-	 *  TASK_LOOPがマクロ定義されている場合，測定せずに，TASK_LOOPに定
-	 *  義された値を，タスク内でのループ回数とする．
-	 *
-	 *  MEASURE_TWICEがマクロ定義されている場合，1回目の測定結果を捨て
-	 *  て，2回目の測定結果を使う．1回目の測定は長めの時間が出るため．
-	 */
-#ifdef TASK_LOOP
-	task_loop = TASK_LOOP;
-#else /* TASK_LOOP */
-
-#ifdef MEASURE_TWICE
-	task_loop = LOOP_REF;
-	SVC_PERROR(get_tim(&stime1));
-	for (i = 0; i < task_loop; i++);
-	SVC_PERROR(get_tim(&stime2));
-#endif /* MEASURE_TWICE */
-
-	task_loop = LOOP_REF;
-	SVC_PERROR(get_tim(&stime1));
-	for (i = 0; i < task_loop; i++);
-	SVC_PERROR(get_tim(&stime2));
-	task_loop = LOOP_REF * 400UL / (stime2 - stime1);
-
-#endif /* TASK_LOOP */
-	tex_loop = task_loop / 5;
-
-	/*
- 	 *  タスクの起動
-	 */
-	SVC_PERROR(act_tsk(TASK1));
-	SVC_PERROR(act_tsk(TASK2));
-	SVC_PERROR(act_tsk(TASK3));
 
 	/*
  	 *  メインループ
 	 */
-	do {
-		SVC_PERROR(serial_rea_dat(TASK_PORTID, &c, 1));
-		switch (c) {
-		case 'e':
-		case 's':
-		case 'S':
-		case 'd':
-		case 'y':
-		case 'Y':
-		case 'z':
-		case 'Z':
-			message[tskno-1] = c;
-			break;
-		case '1':
-			tskno = 1;
-			tskid = TASK1;
-			break;
-		case '2':
-			tskno = 2;
-			tskid = TASK2;
-			break;
-		case '3':
-			tskno = 3;
-			tskid = TASK3;
-			break;
-		case 'a':
-			syslog(LOG_INFO, "#act_tsk(%d)", tskno);
-			SVC_PERROR(act_tsk(tskid));
-			break;
-		case 'A':
-			syslog(LOG_INFO, "#can_act(%d)", tskno);
-			SVC_PERROR(ercd = can_act(tskid));
-			if (ercd >= 0) {
-				syslog(LOG_NOTICE, "can_act(%d) returns %d", tskno, ercd);
-			}
-			break;
-		case 't':
-			syslog(LOG_INFO, "#ter_tsk(%d)", tskno);
-			SVC_PERROR(ter_tsk(tskid));
-			break;
-		case '>':
-			syslog(LOG_INFO, "#chg_pri(%d, HIGH_PRIORITY)", tskno);
-			SVC_PERROR(chg_pri(tskid, HIGH_PRIORITY));
-			break;
-		case '=':
-			syslog(LOG_INFO, "#chg_pri(%d, MID_PRIORITY)", tskno);
-			SVC_PERROR(chg_pri(tskid, MID_PRIORITY));
-			break;
-		case '<':
-			syslog(LOG_INFO, "#chg_pri(%d, LOW_PRIORITY)", tskno);
-			SVC_PERROR(chg_pri(tskid, LOW_PRIORITY));
-			break;
-		case 'G':
-			syslog(LOG_INFO, "#get_pri(%d, &tskpri)", tskno);
-			SVC_PERROR(ercd = get_pri(tskid, &tskpri));
-			if (ercd >= 0) {
-				syslog(LOG_NOTICE, "priority of task %d is %d", tskno, tskpri);
-			}
-			break;
-		case 'w':
-			syslog(LOG_INFO, "#wup_tsk(%d)", tskno);
-			SVC_PERROR(wup_tsk(tskid));
-			break;
-		case 'W':
-			syslog(LOG_INFO, "#can_wup(%d)", tskno);
-			SVC_PERROR(ercd = can_wup(tskid));
-			if (ercd >= 0) {
-				syslog(LOG_NOTICE, "can_wup(%d) returns %d", tskno, ercd);
-			}
-			break;
-		case 'l':
-			syslog(LOG_INFO, "#rel_wai(%d)", tskno);
-			SVC_PERROR(rel_wai(tskid));
-			break;
-		case 'u':
-			syslog(LOG_INFO, "#sus_tsk(%d)", tskno);
-			SVC_PERROR(sus_tsk(tskid));
-			break;
-		case 'm':
-			syslog(LOG_INFO, "#rsm_tsk(%d)", tskno);
-			SVC_PERROR(rsm_tsk(tskid));
-			break;
-		case 'x':
-			syslog(LOG_INFO, "#ras_tex(%d, 0x0001U)", tskno);
-			SVC_PERROR(ras_tex(tskid, 0x0001U));
-			break;
-		case 'X':
-			syslog(LOG_INFO, "#ras_tex(%d, 0x0002U)", tskno);
-			SVC_PERROR(ras_tex(tskid, 0x0002U));
-			break;
-		case 'r':
-			syslog(LOG_INFO, "#rot_rdq(three priorities)");
-			SVC_PERROR(rot_rdq(HIGH_PRIORITY));
-			SVC_PERROR(rot_rdq(MID_PRIORITY));
-			SVC_PERROR(rot_rdq(LOW_PRIORITY));
-			break;
-		case 'c':
-			syslog(LOG_INFO, "#sta_cyc(1)");
-			SVC_PERROR(sta_cyc(CYCHDR1));
-			break;
-		case 'C':
-			syslog(LOG_INFO, "#stp_cyc(1)");
-			SVC_PERROR(stp_cyc(CYCHDR1));
-			break;
-		case 'b':
-			syslog(LOG_INFO, "#sta_alm(1, 5000)");
-			SVC_PERROR(sta_alm(ALMHDR1, 5000));
-			break;
-		case 'B':
-			syslog(LOG_INFO, "#stp_alm(1)");
-			SVC_PERROR(stp_alm(ALMHDR1));
-			break;
-
-		case 'V':
-#ifdef TOPPERS_SUPPORT_GET_UTM
-			SVC_PERROR(get_utm(&utime1));
-			SVC_PERROR(get_utm(&utime2));
-			syslog(LOG_NOTICE, "utime1 = %ld, utime2 = %ld",
-										(ulong_t) utime1, (ulong_t) utime2);
-#else /* TOPPERS_SUPPORT_GET_UTM */
-			syslog(LOG_NOTICE, "get_utm is not supported.");
-#endif /* TOPPERS_SUPPORT_GET_UTM */
-			break;
-
-		case 'v':
-			SVC_PERROR(syslog_msk_log(LOG_UPTO(LOG_INFO),
-										LOG_UPTO(LOG_EMERG)));
-			break;
-		case 'q':
-			SVC_PERROR(syslog_msk_log(LOG_UPTO(LOG_NOTICE),
-										LOG_UPTO(LOG_EMERG)));
-			break;
-
-#ifdef BIT_KERNEL
-		case ' ':
-			SVC_PERROR(loc_cpu());
-			{
-				extern ER	bit_kernel(void);
-
-				SVC_PERROR(ercd = bit_kernel());
-				if (ercd >= 0) {
-					syslog(LOG_NOTICE, "bit_kernel passed.");
-				}
-			}
-			SVC_PERROR(unl_cpu());
-			break;
-#endif /* BIT_KERNEL */
-
-		default:
-			break;
-		}
-	} while (c != '\003' && c != 'Q');
+	while (1)
+	{
+		break;
+	}
 
 	syslog(LOG_NOTICE, "Sample program ends.");
 	SVC_PERROR(ext_ker());
